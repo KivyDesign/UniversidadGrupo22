abceaf015e8246ac50d2eabed7d1bf52817b7c46
Guido Sosa <todoelectrosolucion@gmail.com>
2023-09-07 19:49:21 -0300

    cambios

---
+++ src/UniversidadGrupo22/vistas/UniversidadGrupo22.java
@@ -0,0 +1,13 @@
+package UniversidadGrupo22.vistas;
+
+/**
+ *
+ * @author javier
+ */
+public class UniversidadGrupo22 {
+
+    public static void main(String[] args) {
+        
+    }
+    
+}
--- src/guia2ejercicio04/Categoria.java
+++
@@ -1,7 +0,0 @@
-package guia2ejercicio04;
-
-public enum Categoria {
-    COMESTIBLE,
-    LIMPIEZA,
-    PERFUMERIA
-}
