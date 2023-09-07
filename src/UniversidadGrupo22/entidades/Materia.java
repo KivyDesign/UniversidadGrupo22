b1d78fc6e0edbbb6ad4f18c6ec3116979b6a529b
Guido Sosa <todoelectrosolucion@gmail.com>
2023-09-07 19:54:40 -0300

    ...

---
+++ src/UniversidadGrupo22/entidades/Materia.java
@@ -0,0 +1,10 @@
+package UniversidadGrupo22.entidades;
+
+public class Materia {
+    private int idMateria;
+    private String nombre;
+    private int anioMateria;
+    private boolean activo;
+    
+    
+}
