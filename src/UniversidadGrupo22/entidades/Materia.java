5cf5797d88bba892cec2fa815940acc0f54dc8e3
Guido Sosa <todoelectrosolucion@gmail.com>
2023-09-07 19:57:07 -0300

    ...

--- src/UniversidadGrupo22/entidades/Materia.java
+++ src/UniversidadGrupo22/entidades/Materia.java
@@ -5,6 +5,15 @@ public class Materia {
     private String nombre;
     private int anioMateria;
     private boolean activo;
+
+    public Materia() {
+    }
+
+    public Materia(String nombre, int anioMateria, boolean activo) {
+        this.nombre = nombre;
+        this.anioMateria = anioMateria;
+        this.activo = activo;
+    }
     
     
 }
