ab41aeae7d205d656044fe2a274e7ff8a62771de
Guido Sosa <todoelectrosolucion@gmail.com>
2023-09-07 20:00:01 -0300

    ...

--- src/UniversidadGrupo22/entidades/Materia.java
+++ src/UniversidadGrupo22/entidades/Materia.java
@@ -14,6 +14,43 @@ public class Materia {
         this.anioMateria = anioMateria;
         this.activo = activo;
     }
+
+    public int getIdMateria() {
+        return idMateria;
+    }
+
+    public void setIdMateria(int idMateria) {
+        this.idMateria = idMateria;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getAnioMateria() {
+        return anioMateria;
+    }
+
+    public void setAnioMateria(int anioMateria) {
+        this.anioMateria = anioMateria;
+    }
+
+    public boolean isActivo() {
+        return activo;
+    }
+
+    public void setActivo(boolean activo) {
+        this.activo = activo;
+    }
+
+    @Override
+    public String toString() {
+        return "Materia{" + "idMateria=" + idMateria + ", nombre=" + nombre + ", anioMateria=" + anioMateria + ", activo=" + activo + '}';
+    }
     
     
 }
