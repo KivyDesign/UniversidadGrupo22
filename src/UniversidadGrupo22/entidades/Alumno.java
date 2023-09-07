af965b8231399dcaf776eae4258df64b7304b114
germanesalvatierra <ge22679575@gmail.com>
2023-09-07 20:00:16 -0300

    ...

---
+++ src/UniversidadGrupo22/entidades/Alumno.java
@@ -0,0 +1,88 @@
+
+package universidadejemplo.Entidades;
+
+import java.sql.Date;
+
+
+public class Alumno {
+    private int idAlumno;
+    private int dni;
+    private String apellido;
+    private String nombre;
+    private Date fechaNacimiento;
+    private boolean estado;
+
+    public Alumno() {
+    }
+
+    public Alumno(int idAlumno, int dni, String apellido, String nombre, Date fechaNacimiento, boolean estado) {
+        this.idAlumno = idAlumno;
+        this.dni = dni;
+        this.apellido = apellido;
+        this.nombre = nombre;
+        this.fechaNacimiento = fechaNacimiento;
+        this.estado = estado;
+    }
+
+    public Alumno(int dni, String apellido, String nombre, Date fechaNacimiento, boolean estado) {
+        this.dni = dni;
+        this.apellido = apellido;
+        this.nombre = nombre;
+        this.fechaNacimiento = fechaNacimiento;
+        this.estado = estado;
+    }
+
+    public int getIdAlumno() {
+        return idAlumno;
+    }
+
+    public void setIdAlumno(int idAlumno) {
+        this.idAlumno = idAlumno;
+    }
+
+    public int getDni() {
+        return dni;
+    }
+
+    public void setDni(int dni) {
+        this.dni = dni;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public Date getFechaNacimiento() {
+        return fechaNacimiento;
+    }
+
+    public void setFechaNacimiento(Date fechaNacimiento) {
+        this.fechaNacimiento = fechaNacimiento;
+    }
+
+    public boolean isEstado() {
+        return estado;
+    }
+
+    public void setEstado(boolean estado) {
+        this.estado = estado;
+    }
+
+    @Override
+    public String toString() {
+        return "Alumno{" + "idAlumno=" + idAlumno + ", dni=" + dni + ", apellido=" + apellido + ", nombre=" + nombre + ", fechaNacimiento=" + fechaNacimiento + ", estado=" + estado + '}';
+    }
+    
+}
---
+++ src/UniversidadGrupo22/entidades/Inscripcion.java
@@ -0,0 +1,115 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package universidadejemplo.Entidades;
+
+/**
+ *
+ * @author german
+ */
+public class Inscripcion {
+   private int idInscripto;
+    private int idAlumno;
+    private String alumno;
+    private int idMateria;
+    private String materia;private int nota;
+
+    public Inscripcion(int idInscripto, int idAlumno, String alumno, int idMateria, String materia, int nota) {
+        this.idInscripto = idInscripto;
+        this.idAlumno = idAlumno;
+        this.alumno = alumno;
+        this.idMateria = idMateria;
+        this.materia = materia;
+        this.nota = nota;
+    }
+    
+    
+
+    public Inscripcion(int idInscripto, int idAlumno, int idMateria, int nota) {
+        this.idInscripto = idInscripto;
+        this.idAlumno = idAlumno;
+        this.idMateria = idMateria;
+        this.nota = nota;
+        
+    }
+
+    public Inscripcion(int idAlumno, int idMateria, int nota) {
+        this.idAlumno = idAlumno;
+        this.idMateria = idMateria;
+        this.nota = nota;
+    }
+
+    public Inscripcion(String alumno) {
+        this.alumno = alumno;
+    }
+
+ 
+    
+    
+
+    public Inscripcion() {
+    }
+
+    public int getIdInscripto() {
+        return idInscripto;
+    }
+
+    public void setIdInscripto(int idInscripto) {
+        this.idInscripto = idInscripto;
+    }
+
+    public int getIdAlumno() {
+        return idAlumno;
+    }
+
+    public void setIdAlumno(int idAlumno) {
+        this.idAlumno = idAlumno;
+    }
+
+    public int getIdMateria() {
+        return idMateria;
+    }
+
+    public void setIdMateria(int idMateria) {
+        this.idMateria = idMateria;
+    }
+
+    public int getNota() {
+        return nota;
+    }
+
+    public void setNota(int nota) {
+        this.nota = nota;
+    }
+
+    public String getAlumno() {
+        return alumno;
+    }
+
+    public void setAlumno(String alumno) {
+        this.alumno = alumno;
+    }
+
+    public String getMateria() {
+        return materia;
+    }
+
+    public void setMateria(String materia) {
+        this.materia = materia;
+    }
+
+    @Override
+    public String toString() {
+        return "Inscripcion{" + "idInscripto=" + idInscripto + ", alumno=" + alumno + ", materia=" + materia + ", nota=" + nota + '}';
+    }
+
+      public String listarNotasMayores() {
+        return alumno;
+    }
+
+    
+}
+ 
+
