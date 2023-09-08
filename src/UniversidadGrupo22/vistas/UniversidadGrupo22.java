a37e506096ee01c67e654db839e2bf90b3e4c0b4
Guido Sosa <todoelectrosolucion@gmail.com>
2023-09-08 20:09:29 -0300

    Probando Driver mariadb

--- src/UniversidadGrupo22/vistas/UniversidadGrupo22.java
+++ src/UniversidadGrupo22/vistas/UniversidadGrupo22.java
@@ -1,8 +1,13 @@
 package UniversidadGrupo22.vistas;
 
+import UniversidadGrupo22.accesoADatos.Conexion;
+import java.sql.Connection;
+
 public class UniversidadGrupo22 {
 
     public static void main(String[] args) {
+        Connection con = Conexion.getConexion();
+        
         
     }
     
