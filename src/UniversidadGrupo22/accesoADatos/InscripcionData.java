efac1199ce63ef4f10ae807e4f6a635466325d1c
arielMilton <arielmilton98@gmail.com>
2023-09-11 18:58:40 -0300

    actualizando inscripcionData

--- src/UniversidadGrupo22/accesoADatos/InscripcionData.java
+++ src/UniversidadGrupo22/accesoADatos/InscripcionData.java
@@ -5,10 +5,45 @@
  */
 package UniversidadGrupo22.accesoADatos;
 
+import UniversidadGrupo22.entidades.Inscripcion;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import javax.swing.JOptionPane;
+
 /**
  *
  * @author german
  */
 public class InscripcionData {
+    private Connection con=null;
+    private MateriaData matData;
+    private AlumnoData aluData;
     
+    public InscripcionData(){
+        con=Conexion.getConexion();
+    }
+    
+    public void guardarInscripcion(Inscripcion ins) {
+        String sql = "INSERT INTO inscripcion (idAlumno, idMateria, nota) VALUES (?, ?, ?)";
+
+        try {
+            PreparedStatement ps = con.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+            ps.setInt(1, ins.getAlumno().getIdAlumno());
+            ps.setInt(2, ins.getMateria().getIdMateria());
+            ps.setDouble(3, ins.getNota());
+            ps.executeUpdate();
+            ResultSet rs = ps.getGeneratedKeys();
+
+            if (rs.next()) {
+                ins.setIdInscripcion(rs.getInt(1));
+                JOptionPane.showMessageDialog(null, "Inscripción exitosa");
+            }
+            ps.close();
+        } catch (SQLException ex) {
+            JOptionPane.showMessageDialog(null, "Error al acceder a la tabla inscripcion: " + ex.getMessage());
+        }
+    }
 }
